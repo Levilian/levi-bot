935c7c05c3fdba9bc10802330ce95ec6a41a5e83
Levi Lian <levilian@levis-air.mynetworksettings.com>
2023-05-19 14:02:19 -0400

    seo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import Head from 'next/head';
 
 interface LayoutProps {
   children: ReactNode;
@@ -7,7 +8,25 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div>
-      <header>{/* test */}</header>
+      <Head>
+        <title>
+          Levi Bot: Talks like Levi but Knows Nothing About Him
+        </title>
+        <meta
+          name="description"
+          content="Check out Levi Bot, a chatbot trained on Levi&apos;s real chat history."
+          key="desc"
+        />
+        <meta property="og:title" content="Levi Bot: Mimicking Levi's Talking Style" />
+        <meta
+          property="og:description"
+          content="ChatGPT trained on Levi&apos;s real chat history."
+        />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/x8jRrvy/Levi-high-resolution-color-logo.png"
+        />
+      </Head>
 
       <main>{children}</main>
 
