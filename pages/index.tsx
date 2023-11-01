c5905a43f5bc51d1fe622a2ff2f2f5d4e3483975
Levi Lian <levi_lian007@163.com>
2023-10-31 22:56:00 -0400

    yona.ai link

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ function Home() {
         <hr className='w-full h-1 mt-2 mb-4 border-0 border-t border-gray-400' />
         <h1 className='block pl-1 text-sm tracking-tight text-gray-700'>
            Disclaimer: Although the Levi bot is trained on Levi&apos;s real chat history, everything it says is fake. Take everything with a grain of salt. 
-           However, don&apos;t limit your imagination and have fun chatting! Want to build a chatbot like this? Visit <a href="https://yona-ai.vercel.app">here</a>
+           However, don&apos;t limit your imagination and have fun chatting! Want to build a chatbot like this? Visit <a href="https://yona.ai">here</a>
         </h1>
         <Chat />
       </section>
